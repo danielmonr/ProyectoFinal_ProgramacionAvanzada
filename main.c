8ece9b72309265bc8db31aa5ffb1d1bf62e15ec4
daniel <danielmonr@gmail.com>
2016-04-25 11:14:51 -0500

    Inicio de repositorio

---
+++ main.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  Algoritmo genetico. Ver instrucciones.txt para las especificaciones.
+ *
+ *        Version:  1.0
+ *        Created:  25/04/16 11:07:40
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Daniel Monzalvo (A01021514), Abraham Esses (A01018853), Marcos Lopez (A01020023), Juan Carlos (A01010200)
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+#include    <stdio.h>
+#include    <pthread.h>
+#include    <omp.h>
+
+#define Poblacion 10000
+
+/* ===  FUNCTION MAIN ===================================================================*/
+int main ( int argc, char *argv[] ){
+
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
